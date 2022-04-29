fdb4644088072273737ec0fd5e1a94ac88b0c9d8
Lena Gavrilova <lena.gavrilova2408@mail.ru>
2022-04-29 11:38:06 +0300

    done

--- src/binary_search_recursive.cpp
+++ src/binary_search_recursive.cpp
@@ -13,14 +13,22 @@ namespace assignment {
 
   std::optional<int> BinarySearchRecursive::search(const std::vector<int>& arr, int search_elem, int start, int stop) const {
 
-    // Tips:
-    // 1. Рассмотрите базовые случаи выхода и рекурсии:
-    //    1) индекс левого элемента стал больше индекса правого элемента
-    //    2) целевой элемент найден
-    // 2. Вызовите рекурсивный метод, изменив границы поиска
-    //    в зависимости от неравенства между элементом посередине и целевого элемента
+    if (start>stop){
+      return std::nullopt;
+    }
+    int middle = (start+stop)/2;
+    if(arr[middle]==search_elem){
+      return middle;
+    }
+    if(arr[middle]>search_elem){
+      return search(arr,search_elem,start,middle-1);
+    }
+    if(arr[middle]<search_elem){
+      return search(arr,search_elem,middle+1,stop);
+    }
+
 
     return std::nullopt;
   }
 
-}  // namespace assignment
+}
